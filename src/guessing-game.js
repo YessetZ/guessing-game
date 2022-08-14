936acf40d05b2a2f8d97c2efcae595dccaa97c72
yessetz <yessetkhan@gmail.com>
2022-08-15 00:37:46 +0500

    feat: add guessing-game class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
     constructor() {}
-
+    
     setRange(min, max) {
-
+        this.range = [...Array(max+1).keys()].map(i => i + min);
+        return this.range;
     }
 
     guess() {
-
+        this.index = Math.floor(this.range.length / 2);
+        return this.range[this.index];
     }
 
     lower() {
-
+        let i = Math.floor(this.range.length / 2);
+        let temp = this.range;
+        temp = temp.filter(function(value, index, arr) {
+            return index <= i;
+        })
+        this.range = temp;
     }
 
     greater() {
-
+        let i = Math.floor(this.range.length / 2);
+        let temp = this.range;
+        temp = temp.filter(function(value, index, arr) {
+            return index >= i;
+        })
+        this.range = temp;
     }
 }
 
